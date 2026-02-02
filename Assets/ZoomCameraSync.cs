e493c0dfc79498b5fc3c46916845f72e58602b30
kf9000 <aapo.kois@gmail.com>
2026-02-02 23:02:02 +0200

    Improved magnifying glass

--- Assets/ZoomCameraSync.cs
+++ Assets/ZoomCameraSync.cs
@@ -3,4 +3,14 @@ using UnityEngine;
 public class ZoomCameraSync : MonoBehaviour
 {
 
+    public Transform lens;
+    public Transform xrCam;
+    public Transform lensCam;
+void Update() {
+
+    Vector3 viewDir = lens.position - xrCam.position;
+
+    lensCam.transform.rotation = Quaternion.LookRotation(viewDir);
+}
+
 }
